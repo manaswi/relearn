8022a936851e3d7654024e85f750dba5a584f72a
Manaswi  Sharma <montropistic@gmail.com>
2014-02-25 16:25:30 -0500

    Initialize

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Relearn::Application.config.secret_key_base = '76dc88d8676d64f374a9fe1865a26fb92c9b00d038eddef975c7c3df65d68df9e5925767666a9b587b85a32e7ef0f8b94cc0a6e8e4db018f9abdc54fbdb70de7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Relearn::Application.config.session_store :cookie_store, key: '_relearn_session'
